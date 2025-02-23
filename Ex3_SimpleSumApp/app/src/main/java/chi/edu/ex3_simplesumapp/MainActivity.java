5d07e96e167977c4605c0bce150957a2b1382d29
quynhchiuce <chi.ttq.64cntt@ntu.edu.vn>
2025-02-23 20:44:57 +0700

    Ex3_SimpleSumApp
    Xử lý backend: Bắt/Đáp ứng sự kiện bằng hàm xử lý cài đặt cho OnClick trên XML

--- Ex3_SimpleSumApp/app/src/main/java/chi/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/chi/edu/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,9 @@
 package chi.edu.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;3
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //Gan layout tuong ung voi file nay
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    //Day la bo lang nghe va xu ly su kien click tren nut Tinh Tong
+    public void XuLyCong(View view){
+        //Tim, tham chieu dieu khien tren tem XML
+       EditText editTextsoA = findViewById(R.id.edtA);
+       EditText editTextsoB = findViewById(R.id.edtB);
+       EditText editTextsoKq = findViewById(R.id.edtKq);
+       //Lay du lieu ve o dieu kien so a
+        String strA = editTextsoA.getText().toString(); //strA = "2"
+        //Lay du lieu o dieu kien so b
+        String strB = editTextsoB.getText().toString(); //strB = "4"
+
+        //chuyen du lieu sang dang so
+        int so_A = Integer.parseInt(strA); //2
+        int so_B = Integer.parseInt(strB); //4
+
+        //Tinh toan theo yeu cau
+        int tong =so_A + so_B;
+        String strTong = String.valueOf(tong); //chuyen sang dang chuoi: "6"
+
+        //Hien ra man hinh
+        editTextsoKq.setText(strTong); //6
+
     }
 }
