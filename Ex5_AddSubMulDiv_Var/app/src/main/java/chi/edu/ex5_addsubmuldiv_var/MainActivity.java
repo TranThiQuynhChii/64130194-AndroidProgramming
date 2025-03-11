05461cc8d2eefa47c05ed9890acc60156519b403
quynhchiuce <chi.ttq.64cntt@ntu.edu.vn>
2025-03-11 21:05:55 +0700

    Ex5_AddSubMulDiv_Var

--- Ex5_AddSubMulDiv_Var/app/src/main/java/chi/edu/ex5_addsubmuldiv_var/MainActivity.java
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/chi/edu/ex5_addsubmuldiv_var/MainActivity.java
@@ -20,10 +20,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TimView();
+        btnCong.setOnClickListener(boLangNghe_XuLyCong);
+        btnTru.setOnClickListener(boLangNghe_XuLyTru);
+        btnTru.setOnClickListener(boLangNghe_XuLyNhan);
+        //btnTru.setOnClickListener(boLangNghe_XuLyChia);
+        //ví dụ bộ lắng nghe ẩn danh
+        btnChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code xu ly cong o day
+                //b1.Lay so 1
+                String strSo1 = edtSoA.getText().toString();
+                String strSo2 = edtSoB.getText().toString();
+                //b2.chuyen thanh so de tinh toan
+                double soA = Double.parseDouble(strSo1);
+                double soB = Double.parseDouble(strSo2);
+                //b3.Tinh toan
+                double tong = soA / soB;
+                //b4.Xuat
+                String strKQ = String.valueOf(tong);
+                tvKQ.setText(strKQ);
+            }
+        });
         }
         //gan cac bo lang nghe
-        btnCong.setOnClickListener(boLangNghe_XULyCong);
-        btnTru.set
+
 
         //-----------------------------------
         //Tao cac bo nghe va xu ly su kien
@@ -46,14 +67,51 @@ public class MainActivity extends AppCompatActivity {
         };
 
         //-----------------------------------
-        void TimView(){
+        View.OnClickListener boLangNghe_XuLyTru = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code xu ly cong o day
+                //b1.Lay so 1
+                String strSo1 = edtSoA.getText().toString();
+                String strSo2 = edtSoB.getText().toString();
+                //b2.chuyen thanh so de tinh toan
+                double soA = Double.parseDouble(strSo1);
+                double soB = Double.parseDouble(strSo2);
+                //b3.Tinh toan
+                double tong = soA - soB;
+                //b4.Xuat
+                String strKQ = String.valueOf(tong);
+                tvKQ.setText(strKQ);
+            }
+        };
+
+    View.OnClickListener boLangNghe_XuLyNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //code xu ly cong o day
+            //b1.Lay so 1
+            String strSo1 = edtSoA.getText().toString();
+            String strSo2 = edtSoB.getText().toString();
+            //b2.chuyen thanh so de tinh toan
+            double soA = Double.parseDouble(strSo1);
+            double soB = Double.parseDouble(strSo2);
+            //b3.Tinh toan
+            double tong = soA * soB;
+            //b4.Xuat
+            String strKQ = String.valueOf(tong);
+            tvKQ.setText(strKQ);
+        }
+    };
+
+        public void TimView(){
             //Tim view
             edtSoA = (EditText) findViewById(R.id.editTextA);
             edtSoB = (EditText) findViewById(R.id.editTextB);
-            btnCong = (EditText) findViewById(R.id.buttonCong);
-            btnTru = (EditText) findViewById(R.id.buttonTru);
-            btnNhan = (EditText) findViewById(R.id.buttonNhan);
-            btnChia = (EditText) findViewById(R.id.buttonChia);
+            btnCong = (Button) findViewById(R.id.buttonCong);
+            btnTru = (Button) findViewById(R.id.buttonTru);
+            btnNhan = (Button) findViewById(R.id.buttonNhan);
+            btnChia = (Button) findViewById(R.id.buttonChia);
+            tvKQ = (TextView)  findViewById(R.id.tvKQ);
         }
     //khai bao ca doi tuong tuong ung voi cac dieu khien (view) can thao tac
     EditText edtSoA;
