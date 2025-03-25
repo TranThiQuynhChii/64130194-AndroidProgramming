71bd500ff330af81563980ec932c3a43b8890cde
quynhchiuce <chi.ttq.64cntt@ntu.edu.vn>
2025-03-25 07:36:45 +0700

    LUYENTAP

--- LUYENTAP/app/src/main/java/chi/edu/luyentap/MainActivity.java
+++ LUYENTAP/app/src/main/java/chi/edu/luyentap/MainActivity.java
@@ -12,21 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //Tim dk
-        Button btnNhan = (Button) findViewById(R.id.btn);
-        //su kien
-        btnNhan.setOnClickListener(new View.OnClickListener() {
+        //tim dk
+        Button btnDN = (Button) findViewById(R.id.btn);
+        //xu ly d
+        btnDN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //xu ly lenh o day
-                Intent imain = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(imain);
+                Intent imain2 = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(imain2);
             }
         });
     }
